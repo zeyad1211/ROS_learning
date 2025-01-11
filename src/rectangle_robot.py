9c3330525ca27bdebe91f0f96006b5378900530a
zeyad <zeyadwagih@gmail.com>
2025-01-11 16:26:12 +0800

    version2

--- src/rectangle_robot.py
+++ src/rectangle_robot.py
@@ -10,23 +10,30 @@ y_pos = 0
 theta_pos = 0
 
 
-def callback_func():
+def callback_func(data):
     global x_pos, y_pos, theta_pos
     
-    x_pos = Pose.x
-    y_pos = Pose.y
-    theta_pos = Pose.theta
+    x_pos = data.x
+    y_pos = data.y
+    theta_pos = data.theta
 
 
 def rotate90_clockwise():
+    global x_pos, theta_pos
     velocity_message = Twist()
     velocity_message.angular.z = -2
-    loop_rate = rospy.Rate(10)
+    loop_rate = rospy.Rate(10000)
     command = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-    angle_desired = 1.5708
+    angle_desired = 90
+    initialAngle = (theta_pos) * (180/math.pi)
+    while not rospy.is_shutdown():
+        angle_rotated = abs(((theta_pos) * (180/math.pi)) - initialAngle)
+        rospy.loginfo(angle_rotated)
 
-    while theta_pos < angle_desired:
-        rospy.loginfo('turtle turning clockwise 90')
+        if angle_rotated >= abs(angle_desired):
+            rospy.loginfo("target angle reached...")
+            break
+
         command.publish(velocity_message)
         loop_rate.sleep()
     
@@ -34,24 +41,38 @@ def rotate90_clockwise():
     command.publish(velocity_message)
 
 def move_forward(distance):
+    global x_pos, y_pos, theta_pos
     velocity_message = Twist()
     velocity_message.linear.x = 2
     loop_rate = rospy.Rate(10)
     command = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     current_pos = x_pos
-    
-    while x_pos - current_pos < distance:
-        rospy.loginfo("turtle moving forward")
+    # Calculate the initial position
+    start_x = x_pos
+    start_y = y_pos
+    while not rospy.is_shutdown():
+        # Compute the distance moved
+        distance_moved = ((x_pos - start_x) ** 2 + (y_pos - start_y) ** 2) ** 0.5
+        # rospy.loginfo(distance_moved)
+        # Stop when the turtle has moved the required distance
+        if distance_moved >= distance: 
+            rospy.loginfo("Target distance reached. Stopping turtle.")
+            break
+
+        # Publish the velocity message
         command.publish(velocity_message)
         loop_rate.sleep()
 
-    velocity_message.linear.x = 0
+    # Stop the turtle
+    velocity_message.linear.x = 0.0
     command.publish(velocity_message)
 
 if __name__ == '__main__':
     try:
         rospy.init_node('rectange_robot_pub', anonymous=True)
         rospy.Subscriber('/turtle1/pose', Pose, callback_func)
+        time.sleep(1.0)
+
         move_forward(4)
         time.sleep(1.0)
         rotate90_clockwise()
